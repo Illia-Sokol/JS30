3ae363fa25f4126c107014b5a7a95cbd14c2f784
Illia Sokol <illiasokolovskyi@gmail.com>
2016-12-30 02:22:59 +0200

    added functional toggle Play button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,9 +5,19 @@ function playVieo() {
     video[match]();
 }
 
+function changeButton() {
+    // console.log('btn')
+    const btn = video.paused ? '►' : 'l l';
+    button.textContent = btn;
+}
+
 // Select element
 const video = document.querySelector('.viewer');
 const progress = document.querySelector('.progress');
+const button = document.querySelector('.toggle');
 
 
 video.addEventListener('click', playVieo);
+video.addEventListener('play', changeButton);
+video.addEventListener('pause', changeButton);
+button.addEventListener('click', playVieo);
