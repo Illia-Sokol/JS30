22dd8d46ece86335d1e63fe9020fd9a6dcc3933e
Illia Sokol <illiasokolovskyi@gmail.com>
2016-12-30 02:40:23 +0200

    added slider functional (volume/speed) video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -16,11 +16,17 @@ function skipFuncton(e) {
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function changeValueSpeed(e) {
+    // console.log(this.name);
+    video[this.name] = this.value;
+}
+
 // Select element
 const video = document.querySelector('.viewer');
 const progress = document.querySelector('.progress');
 const button = document.querySelector('.toggle');
 const skips = document.querySelectorAll('.player__button');
+const sliders = document.querySelectorAll('.player__slider');
 //listener
 video.addEventListener('click', playVieo);
 video.addEventListener('play', changeButton);
@@ -28,4 +34,7 @@ video.addEventListener('pause', changeButton);
 button.addEventListener('click', playVieo);
 skips.forEach( skip => {
     skip.addEventListener('click', skipFuncton);   
+})
+sliders.forEach( slider => {
+    slider.addEventListener('click', changeValueSpeed);
 })
