aef585775b474581f81971cf9e9d97f47158f27d
Illia Sokol <illiasokolovskyi@gmail.com>
2016-12-30 02:31:34 +0200

    added skip button functional

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -11,13 +11,21 @@ function changeButton() {
     button.textContent = btn;
 }
 
+function skipFuncton(e) {
+    // console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
 // Select element
 const video = document.querySelector('.viewer');
 const progress = document.querySelector('.progress');
 const button = document.querySelector('.toggle');
-
-
+const skips = document.querySelectorAll('.player__button');
+//listener
 video.addEventListener('click', playVieo);
 video.addEventListener('play', changeButton);
 video.addEventListener('pause', changeButton);
 button.addEventListener('click', playVieo);
+skips.forEach( skip => {
+    skip.addEventListener('click', skipFuncton);   
+})
